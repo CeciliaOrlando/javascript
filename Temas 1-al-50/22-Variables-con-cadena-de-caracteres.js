59963ca6093eaefce90dd03e6824ca9ded7078cd
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-04-03 10:43:11 -0300

    plk

---
+++ Temas 1-al-50/22-Variables-con-cadena-de-caracteres.js
@@ -0,0 +1,12 @@
+/*Tema: Variables con cadena de caracteres. */
+
+let nombre = "Alan";
+
+// Variaciones de la cadena de caracteres
+let nombre = "A lan";
+let nombre = "A&*$%^&#@@lan";
+let nombre = "Alan34534_+-";
+
+// Ejemplo
+
+let apellido = "Turing";

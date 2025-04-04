9b4642d72f606007ffe8f3b30414360157dd798f
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-04-04 11:43:59 -0300

    50

--- Temas 1-al-50/23-Escapar-comillas-cadena-de-caracteres.js
+++ Temas 1-al-50/23-Escapar-comillas-cadena-de-caracteres.js
@@ -1,11 +1,7 @@
 
-/*
-Curso de freeCodeCamp: "Aprende JavaScript - Curso Completo desde Cero".
-Curso Creado por: Estefania Cassingena Navone (@EstefaniaCassN).
-Tema: Escapar comillas en cadenas de caracteres.
-*/
+/* Tema: Escapar comillas en cadenas de caracteres. */
 
 // var miCadena = "Soy una cadena de caracteres "con comillas""; // Error
 
-var miCadena = "Soy una cadena de caracteres \"con comillas\"";
+let miCadena = "Soy una cadena de caracteres \"con comillas\"";
 console.log(miCadena);

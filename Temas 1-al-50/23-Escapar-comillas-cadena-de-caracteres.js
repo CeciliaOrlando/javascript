3f15da4db40de23416cb93c455fd5e56240fdb32
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-04-07 06:14:06 -0300

    Definir commillas

--- Temas 1-al-50/23-Escapar-comillas-cadena-de-caracteres.js
+++ Temas 1-al-50/23-Escapar-comillas-cadena-de-caracteres.js
@@ -1,7 +1,9 @@
 
 /* Tema: Escapar comillas en cadenas de caracteres. */
 
-// var miCadena = "Soy una cadena de caracteres "con comillas""; // Error
+// var miCadena = "Soy una cadena de caracteres "con comillas""; // Error. En este caso, el compilador no sabe si la cadena termina en la primera comilla o en la segunda, por eso da error. Para substituir las comillas dobles por comillas simples, se puede usar la barra invertida (\) para escapar las comillas dobles. De esta manera, el compilador no confundirá las comillas de la cadena con las comillas que delimitan la cadena.
+
+// miCadena = "Soy una cadena de caracteres \"con comillas\""; // Opción previa
 
 let miCadena = "Soy una cadena de caracteres \"con comillas\"";
-console.log(miCadena);
+console.log(miCadena); // "Soy una cadena de caracteres "con comillas""
--- Temas 1-al-50/24-Definir-cadena-con-comillas-simples.js
+++ Temas 1-al-50/24-Definir-cadena-con-comillas-simples.js
@@ -1,5 +1,5 @@
 /* Tema: Definir cadenas con comillas simples.
-*/
+Son dos fotmas de usar distintos tipos de commilas */
 
 let  miMeta;
 
@@ -9,8 +9,8 @@ miMeta = "Aprender a programar con \"freeCodeCamp\""; // Opción previa
 
 // Alternativa más concisa: usar otro tipo de comillas.
 
-miMeta = 'Aprender a programar con "freeCodeCamp"';
+miMeta = 'Aprender a programar con "freeCodeCamp"'; // Opción 1 commilas simples en el string principal y dobles dentro de la cadena
 
-miMeta = "Aprender a programar con 'freeCodeCamp'";
+miMeta = "Aprender a programar con 'freeCodeCamp'"; // Opción 2 comillas dobles en el string principal y simples dentro de la cadena
 
 console.log(miMeta);

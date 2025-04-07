29aa7751e5407df32fa288603950995863ab8f65
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-04-07 06:16:30 -0300

    Secuencias de escape

--- Temas 1-al-50/25-Secuencia-de-escape.js
+++ Temas 1-al-50/25-Secuencia-de-escape.js
@@ -1,20 +1,18 @@
-/*
-Curso de freeCodeCamp: "Aprende JavaScript - Curso Completo desde Cero".
-Curso Creado por: Estefania Cassingena Navone (@EstefaniaCassN).
-Tema: Secuencias de escape en JavaScript.
+/* Tema: Secuencias de escape en JavaScript.
+Formas de representar caracteres especiales dentro de cadenas de texto.
 */
 
 /*
 Código | Resultado
 ------------------
-\'      Comilla Simple
-\"      Comilla Doble
-\\      Barra invertida
-\n      Línea Nueva
-\r      Retorno de Carro
-\t      Tabulación
-\b      Retroceso
-\f      Salto de Página
+\'      Comilla Simple  // se usa para incluir comillas simples dentro de una cadena delimitada por comillas simples.
+\"      Comilla Doble  // se usa para incluir comillas dobles dentro de una cadena delimitada por comillas dobles.
+\\      Barra invertida  // se usa para incluir una barra invertida dentro de una cadena.
+\n      Línea Nueva  // se usa para incluir un salto de línea dentro de una cadena.
+\r      Retorno de Carro  // se usa para mover el cursor al inicio de la línea.
+\t      Tabulación // se usa para incluir un tabulador dentro de una cadena.
+\b      Retroceso  // se usa para eliminar el carácter anterior.
+\f      Salto de Página  // se usa para incluir un salto de página dentro de una cadena.
 ...
  */
 

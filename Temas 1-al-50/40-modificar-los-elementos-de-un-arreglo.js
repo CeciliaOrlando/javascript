541b7e2e904d4df6c5bcc0075ae396c116fe3ade
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-04-09 05:54:19 -0300

    arrays multidimencionales

--- Temas 1-al-50/40-modificar-los-elementos-de-un-arreglo.js
+++ Temas 1-al-50/40-modificar-los-elementos-de-un-arreglo.js
@@ -5,11 +5,11 @@
 var miArreglo = [10, 20, 30];
 
 console.log(miArreglo); // 10, 20, 30
-miArreglo[0] = 40;
+miArreglo[0] = 40; // modificar el primer elemento del arreglo
 console.log(miArreglo); // 40, 20, 30
 
-miArreglo[1] = "Hola";
+miArreglo[1] = "Hola"; // modificar el segundo elemento del arreglo
 console.log(miArreglo); // 40, Hola, 30
 
-miArreglo[1] = [1, 2, 3];
+miArreglo[1] = [1, 2, 3]; // modificar el segundo elemento del arreglo
 console.log(miArreglo); // 40, 1, 2, 3, 30
--- Temas 1-al-50/41-acceder-a-arreglos-multidimensionales.js
+++ Temas 1-al-50/41-acceder-a-arreglos-multidimensionales.js
@@ -1,10 +1,15 @@
-/*
-Curso de freeCodeCamp: "Aprende JavaScript - Curso Completo desde Cero".
-Curso Creado por: Estefania Cassingena Navone (@EstefaniaCassN).
-Tema: Acceder a arreglos multidimensionales.
-*/
+/* Tema: Acceder a arreglos multidimensionales.
+Para acceder a los elementos de un arreglo multidimensional, se utilizan dos pares de corchetes.
+El primer par de corchetes indica el índice del arreglo externo, y el segundo par de corchetes indica el índice del arreglo interno.
+Por ejemplo, si tenemos un arreglo multidimensional llamado "miArreglo", podemos acceder a sus elementos de la siguiente manera:
+miArreglo[0] // Accede al primer arreglo interno
+miArreglo[0][0] // Accede al primer elemento del primer arreglo interno
+miArreglo[1] // Accede al segundo arreglo interno
+miArreglo[1][0] // Accede al primer elemento del segundo arreglo interno
+miArreglo[2] // Accede al tercer arreglo interno
+miArreglo[2][0] // Accede al primer elemento del tercer arreglo interno*/
 
-var miArreglo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let  miArreglo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
 /*
 Arreglo:          [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
